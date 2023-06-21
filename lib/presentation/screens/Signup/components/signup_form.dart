8c658ea25080e8767061d4a4dbca240b011a63f2
singh <singh.satnam0@gmail.com>
2023-06-22 00:10:59 +0530

    merge command

--- lib/presentation/screens/Signup/components/signup_form.dart
+++ lib/presentation/screens/Signup/components/signup_form.dart
@@ -421,7 +421,7 @@ class SignUpForm extends StatefulWidget {
     else{
 
       Toast.show("use git merge", duration: Toast.lengthShort, gravity:  Toast.center);
-      Toast.show("use git merge23", duration: Toast.lengthShort, gravity:  Toast.center);
+
 
     }
 
